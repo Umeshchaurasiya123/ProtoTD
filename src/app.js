5e122f534692491daf4157893ecad543c2d4c4ec
Umeshchaurasiya123 <umeshc1998@gmail.com>
2025-06-05 23:19:12 +0530

    chore: add middleware for JSON parsing, URL-encoded data, static file serving, and cookie parsing

--- src/app.js
+++ src/app.js
@@ -8,6 +8,22 @@ app.use(cors({
     credentials:true
 }));
 
+//to pass data from client to server through req.body
+//for JSON data
+app.use(express.json({limit: '10mb'}));
+
+//for form data pass through req.body
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+//to serve static files like images, css, js, etc.
+//public folder is the default folder to serve static files
+app.use(express.static('public'));
+
+
+
+// for stroing and parsing cookies
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 
 module.exports = app;
